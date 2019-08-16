bfed0066e2b6a826504d0bfabbdb9ad17ca1bf8c
Roy Nunez <roynunez273@gmail.com>
2019-08-16 09:48:20 -0400

    Submitting

---
+++ Week1.java
@@ -0,0 +1,19 @@
+
+/* the following code is runtime polymorphism because it is 
+using the overiding concept where we use a different method 
+when we call the method.
+*/
+
+class Week1{
+
+  
+    static void method1 (int one){
+        System.out.println(one);
+    }
+    public static void main(String[] args) {
+
+        method1("one");
+
+    }
+}
+
